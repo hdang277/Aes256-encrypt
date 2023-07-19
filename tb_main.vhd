efffee354049b30077fd32ecbee27882b22432df
hdang277 <117496965+hdang277@users.noreply.github.com>
2023-07-19 08:22:18 +0700

    Update tb_main.vhd

--- tb_main.vhd
+++ tb_main.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: ATAKAN CAKIR
--- 
--- Create Date: 26.10.2022 22:16:41
--- Design Name: 
--- Module Name: tb_main - Behavioral
--- Project Name: Advanced Encryption Standard (AES)
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
